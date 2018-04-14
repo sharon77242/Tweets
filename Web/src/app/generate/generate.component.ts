d3d98a064e934a2d02c9f038f61b82c5b78fbb6a
Alon <alon1853@gmail.com>
2018-04-14 19:16:16 +0300

    Add 'select state' to generate page

--- Web/src/app/generate/generate.component.ts
+++ Web/src/app/generate/generate.component.ts
@@ -6,8 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./generate.component.css']
 })
 export class GenerateComponent implements OnInit {
+  statesList: Array<{ label: string, value: string }>;
+  selectedState: string;
 
-  constructor() { }
+  constructor() {
+    this.selectedState = '';
+
+    this.statesList = [
+      { label: 'New York', value: 'NY' },
+      { label: 'California', value: 'CA' },
+    ];
+  }
 
   ngOnInit() {
   }
